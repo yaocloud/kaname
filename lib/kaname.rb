185dd8867779596318c3e2b7e6c118494699a378
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-04-01 18:35:24 +0900

    load yaml file for definition of keystone

--- lib/kaname.rb
+++ lib/kaname.rb
@@ -1,10 +1,20 @@
 require "kaname/version"
+require 'yaml'
 
 module Kaname
   class CLI
+    DEFAULT_FILENAME = 'keystone.yml'
     class << self
+      def load_yaml
+        if File.exists?(DEFAULT_FILENAME)
+          YAML.load_file(DEFAULT_FILENAME)
+        else
+          ""
+        end
+      end
+
       def run
-        p :foo
+        p load_yaml
       end
     end
   end
