cdd6119cd6a2b0bb837cbd5e7c50f793f9a1118f
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-04-01 18:58:22 +0900

    make newuser

--- lib/kaname.rb
+++ lib/kaname.rb
@@ -28,15 +28,18 @@ module Kaname
 
       def run
         yaml.each do |user,h|
-          begin
-            p users.find_by_name(user)
-          rescue Fog::Identity::OpenStack::NotFound
-            # create new users
-          end
+          id = begin
+                 user = users.find_by_name(user)
+                 user.id
+               rescue Fog::Identity::OpenStack::NotFound
+                 response = Fog::Identity[:openstack].create_user(user, h["password"], h["email"])
+                 response.data[:body]["user"]["id"]
+               end
 
           h["tenants"].each do |tenant, role|
-            p tenants.find{|t| t.name == tenant}
-            p roles.find{|r| r.name == role}
+            tenant = tenants.find{|t| t.name == tenant}
+            role = roles.find{|r| r.name == role}
+            Fog::Identity[:openstack].create_user_role(tenant.id, id, role.id)
           end
         end
       end
