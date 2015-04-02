a1d871c201b8d85ed3951c3803629c0edb5f4bea
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-04-02 18:39:40 +0900

    split test file

--- test/test_kaname.rb
+++ test/test_kaname.rb
@@ -4,14 +4,4 @@ class TestKaname < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Kaname::VERSION
   end
-
-  def test_yaml
-    Dir.chdir "test/fixtures" do
-      assert_equal Hash, Kaname::Resource.yaml.class
-    end
-  end
-
-  def test_yaml_with_no_file
-    refute Kaname::Resource.yaml
-  end
 end
---
+++ test/test_kaname_resource.rb
@@ -0,0 +1,13 @@
+require 'minitest_helper'
+
+class TestKanameResource < Minitest::Test
+  def test_yaml
+    Dir.chdir "test/fixtures" do
+      assert_equal Hash, Kaname::Resource.yaml.class
+    end
+  end
+
+  def test_yaml_with_no_file
+    refute Kaname::Resource.yaml
+  end
+end
