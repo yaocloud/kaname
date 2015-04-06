3dde9b6630dba650f25b45307f41bbd8f82c9808
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-04-06 18:33:27 +0900

    dirty implements

--- lib/kaname/cli.rb
+++ lib/kaname/cli.rb
@@ -27,6 +27,22 @@ module Kaname
               id = Kaname::Resource.user(resource[0])
               Fog::Identity[:openstack].delete_user(id)
             end
+          elsif resource.size == 3 # "user.tenants.foo"
+            id = Kaname::Resource.user(resource[0])
+            tenant = Kaname::Resource.tenants.find{|t| t.name == resource[2]}
+            role = Kaname::Resource.roles.find{|r| r.name == diff[2]}
+            case diff[0]
+            when "+"
+              Fog::Identity[:openstack].create_user_role(tenant.id, id, role.id)
+            when "-"
+              Fog::Identity[:openstack].delete_user_role(tenant.id, id, role.id)
+            when "~"
+              Fog::Identity[:openstack].delete_user_role(tenant.id, id, role.id)
+              role = Kaname::Resource.roles.find{|r| r.name == diff[3]}
+              Fog::Identity[:openstack].create_user_role(tenant.id, id, role.id)
+            end
+          else # "user.tenants"
+            # XXX
           end
         end
       else
