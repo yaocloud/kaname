f7df831bc5865acaa176e3c775f34e34af184ae6
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-04-07 16:43:39 +0900

    added user deletion test

--- test/test_kaname_cli.rb
+++ test/test_kaname_cli.rb
@@ -32,4 +32,14 @@ class TestKanameCLI < Minitest::Test
 
     Kaname::CLI.new.invoke(:apply, [], {dryrun: true})
   end
+
+  def test_delete_user
+    Kaname::Resource.stubs(:yaml).returns(YAML.load_file('test/fixtures/expected_004.yml'))
+
+    Kaname::Adapter::Mock.any_instance.expects(:create_user).once
+    Kaname::Adapter::Mock.any_instance.expects(:create_user_role).once
+    Kaname::Adapter::Mock.any_instance.expects(:delete_user).once
+
+    Kaname::CLI.new.invoke(:apply, [], {dryrun: true})
+  end
 end
