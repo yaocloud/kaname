8ff884ba18102004277609107fc64ccf930be274
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-04-07 16:32:18 +0900

    added twice role test

--- test/test_kaname_cli.rb
+++ test/test_kaname_cli.rb
@@ -1,12 +1,24 @@
 require 'minitest_helper'
 
 class TestKanameCLI < Minitest::Test
-  def test_create_user
+  def setup
     Kaname::Resource.stubs(:users_hash).returns(YAML.load_file('test/fixtures/actual.yml'))
+  end
+
+  def test_create_user
     Kaname::Resource.stubs(:yaml).returns(YAML.load_file('test/fixtures/expected_001.yml'))
 
     Kaname::Adapter::Mock.any_instance.expects(:create_user).once
     Kaname::Adapter::Mock.any_instance.expects(:create_user_role).once
+
+    Kaname::CLI.new.invoke(:apply, [], {dryrun: true})
+  end
+
+  def test_create_user_twice_role
+    Kaname::Resource.stubs(:yaml).returns(YAML.load_file('test/fixtures/expected_002.yml'))
+
+    Kaname::Adapter::Mock.any_instance.expects(:create_user).once
+    Kaname::Adapter::Mock.any_instance.expects(:create_user_role).twice
 
     Kaname::CLI.new.invoke(:apply, [], {dryrun: true})
   end
