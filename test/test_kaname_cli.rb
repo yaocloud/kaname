4fec56e608c14cad57a8fa91033065eae642a74e
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-04-07 16:39:40 +0900

    added tests

--- test/test_kaname_cli.rb
+++ test/test_kaname_cli.rb
@@ -22,4 +22,14 @@ class TestKanameCLI < Minitest::Test
 
     Kaname::CLI.new.invoke(:apply, [], {dryrun: true})
   end
+
+  def test_modify_role
+    Kaname::Resource.stubs(:yaml).returns(YAML.load_file('test/fixtures/expected_003.yml'))
+
+    Kaname::Adapter::Mock.any_instance.expects(:create_user_role).once
+    Kaname::Adapter::Mock.any_instance.expects(:delete_user_role).once
+    Kaname::Adapter::Mock.any_instance.expects(:change_user_role).once
+
+    Kaname::CLI.new.invoke(:apply, [], {dryrun: true})
+  end
 end
