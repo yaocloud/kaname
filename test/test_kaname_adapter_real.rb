0d219ebba7d93a1d519e6536504e3f35154042a8
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-05-12 12:23:29 +0900

    stub request

--- test/test_kaname_adapter_real.rb
+++ test/test_kaname_adapter_real.rb
@@ -2,5 +2,13 @@ require 'minitest_helper'
 
 class TestKanameAdapterReal < Minitest::Test
   def test_update_user_password
+    credentials = {:openstack_current_user_id => "foo", :openstack_auth_token => "bar"}
+
+    stub_request(:patch, "http://api-vip.u01.pbcloud.local:5000/v2.0/OS-KSCRUD/users/foo").
+      with(:body => "{\"user\":{\"password\":\"new\",\"original_password\":\"old\"}}",
+           :headers => {'Content-Type'=>'application/json', 'X-Auth-Token'=>'bar'}).
+      to_return(:status => 200)
+
+    Kaname::Adapter::Real.new.update_user_password(credentials, "old", "new")
   end
 end
