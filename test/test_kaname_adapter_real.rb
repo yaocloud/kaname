809c29a8a4488b9581b1242dae59de3666321ea4
Joe-noh <goflb.jh@gmail.com>
2015-11-06 15:19:26 +0900

    fix test of update_user_password

--- test/test_kaname_adapter_real.rb
+++ test/test_kaname_adapter_real.rb
@@ -2,13 +2,25 @@ require 'minitest_helper'
 
 class TestKanameAdapterReal < Minitest::Test
   def test_update_user_password
-    credentials = {:openstack_current_user_id => "foo", :openstack_auth_token => "bar", :openstack_management_url => "http://www.example.com/v2.0"}
+    mock_auth = MiniTest::Mock.new.expect(:token, 'dummy_token')
+    mock_user = MiniTest::Mock.new.expect(:id, 'dummy_id')
 
-    stub_request(:patch, "http://www.example.com:5000/v2.0/OS-KSCRUD/users/foo").
+    dummy_management_url = "http://www.example.com:5000/v2.0"
+
+    stub_request(:patch, "#{dummy_management_url}/OS-KSCRUD/users/dummy_id").
       with(:body => "{\"user\":{\"password\":\"new\",\"original_password\":\"old\"}}",
-           :headers => {'Content-Type'=>'application/json', 'X-Auth-Token'=>"bar"}).
+           :headers => {'Content-Type'=>'application/json', 'X-Auth-Token'=>"dummy_token"}).
       to_return(:status => 200)
 
-    Kaname::Adapter::Real.new.update_user_password(credentials, "old", "new")
+    Yao::Auth.stub(:try_new, mock_auth) do
+      Yao::User.stub(:get_by_name, mock_user) do
+        Kaname::Config.stub(:management_url, dummy_management_url) do
+          Kaname::Adapter::Real.new.update_user_password('old', 'new')
+        end
+      end
+    end
+
+    mock_auth.verify
+    mock_user.verify
   end
 end
