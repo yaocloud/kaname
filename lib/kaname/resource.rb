d1968f6c05688cf1c5c5ef310ca555a1f040bd2e
suzupy <suzuki.n@pepabo.com>
2017-03-22 19:31:09 +0900

    Add all_tenants configration to keystone
    all_tenants specify authority for all tenants.
    if both tenants and all_tenants are setting, tenants is preferred.

--- lib/kaname/resource.rb
+++ lib/kaname/resource.rb
@@ -4,11 +4,25 @@ module Kaname
   class Resource
     class << self
       def yaml(filename = 'keystone.yml')
-        @_yaml = if File.exists?(filename)
-                   YAML.load_file(filename)
-                 else
-                   nil
-                 end
+        if File.exists?(filename)
+          @_yaml = YAML.load_file(filename)
+          expand_all_tenants
+        else
+          nil
+        end
+      end
+
+      private
+
+      def expand_all_tenants
+        list_tenants = Kaname::Adapter::ReadOnly.new.list_tenants
+
+        @_yaml.each do |username, config|
+          next unless config['all_tenants']
+          tenants = list_tenants.map{|tenant| [tenant.name, config['all_tenants']]}
+          config['tenants'] = Hash[*tenants.flatten].merge(config['tenants'] || {})
+          config.delete('all_tenants')
+        end
       end
     end
   end
--- test/test_kaname_resource.rb
+++ test/test_kaname_resource.rb
@@ -1,9 +1,20 @@
 require 'minitest_helper'
 
 class TestKanameResource < Minitest::Test
+  def setup
+    Kaname::Config.stubs(:setup)
+    @list_tenants = [Yao::Resources::Tenant.new({'name' => 'tenant_a'})]
+  end
+
   def test_yaml
-    Dir.chdir "test/fixtures" do
-      assert_equal Hash, Kaname::Resource.yaml('actual.yml').class
+    Kaname::Adapter::ReadOnly.stub_any_instance(:list_tenants, @list_tenants) do
+      Dir.chdir "test/fixtures" do
+        resource = Kaname::Resource.yaml('actual.yml')
+        assert_equal Hash, resource.class
+        assert_equal 'member', resource['foo']['tenants']['tenant_a']
+        assert_equal 'admin', resource['foo']['tenants']['buzz']
+        assert_nil resource['foo']['all_tenants']
+      end
     end
   end
 
