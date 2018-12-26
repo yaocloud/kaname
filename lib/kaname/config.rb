e778329e44ca94ccbc9ba7c9d096f26a4316e5e7
r_takaishi <ryo.takaishi.0@gmail.com>
2018-12-26 12:26:24 +0900

    align

--- lib/kaname/config.rb
+++ lib/kaname/config.rb
@@ -36,29 +36,29 @@ module Kaname
         raise "Configuration '#{conf_item}' is missing. Check your ~/.kaname" unless config[conf_item]
       end
 
-      @@auth_url       = config['auth_url']
-      @@tenant         = config['tenant']
-      @@username       = config['username']
-      @@password       = config['password']
-      @@management_url = config['management_url']
-      @@client_cert    = config['client_cert']
-      @@client_key     = config['client_key']
-      @@region_name    = config['region_name']
-      @@user_domain_name = config['user_domain_name']
-      @@project_domain_name = config['project_domain_name']
+      @@auth_url             = config['auth_url']
+      @@tenant               = config['tenant']
+      @@username             = config['username']
+      @@password             = config['password']
+      @@management_url       = config['management_url']
+      @@client_cert          = config['client_cert']
+      @@client_key           = config['client_key']
+      @@region_name          = config['region_name']
+      @@user_domain_name     = config['user_domain_name']
+      @@project_domain_name  = config['project_domain_name']
       @@identity_api_version = config['identity_api_version']
       true
     end
 
     def self.setup_yao
       Yao.configure do
-        auth_url    (ENV['OS_AUTH_URL']    || @@auth_url)
-        tenant_name (ENV['OS_TENANT_NAME'] || @@tenant)
-        username    (ENV['OS_USERNAME']    || @@username)
-        password    (ENV['OS_PASSWORD']    || @@password)
-        client_cert (ENV['OS_CERT']        || @@client_cert)
-        client_key  (ENV['OS_KEY']         || @@client_key)
-        region_name (ENV['OS_REGION_NAME'] || @@region_name)
+        auth_url             (ENV['OS_AUTH_URL']             || @@auth_url)
+        tenant_name          (ENV['OS_TENANT_NAME']          || @@tenant)
+        username             (ENV['OS_USERNAME']             || @@username)
+        password             (ENV['OS_PASSWORD']             || @@password)
+        client_cert          (ENV['OS_CERT']                 || @@client_cert)
+        client_key           (ENV['OS_KEY']                  || @@client_key)
+        region_name          (ENV['OS_REGION_NAME']          || @@region_name)
         identity_api_version (ENV['OS_IDENTITY_API_VERSION'] || @@identity_api_version)
         user_domain_name     (ENV['OS_USER_DOMAIN_NAME']     || @@user_domain_name)
         project_domain_name  (ENV['OS_PROJECT_DOMAIN_NAME']  || @@project_domain_name)
